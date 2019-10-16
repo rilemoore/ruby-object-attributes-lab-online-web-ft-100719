a21e76137b8edce8d374fdc337f57524606bef8b
Riley Moore <rilemoore@gmail.com>
2019-10-16 20:22:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,36 @@
 class Dog
   
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
   
-  def breed=
+  def name
+    @this_dogs_name
+  end
+  
+  
+  def breed=(breed_name)
+    @breed = breed_name
+  end
   
   def breed
     @breed
   end
   
-  
 end
 
 class Person
   
+  def name=(persons_name)
+    @this_persons_name = persons_name
+  end
   
+  def name
+    @this_persons_name
+  end
   
-  
+  def job=(jobs_name)
+    @this_persons_job
+  end
   
 end
