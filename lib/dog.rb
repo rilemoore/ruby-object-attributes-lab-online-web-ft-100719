e9fe105432809436b7f632fde73d41210bdef27c
Riley Moore <rilemoore@gmail.com>
2019-10-16 20:23:35 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,43 @@
+class Dog
+  
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+  
+  def name
+    @name
+  end
+  
+  
+  def breed=(breed_name)
+    @breed = breed_name
+  end
+  
+  def breed
+    @breed
+  end
+  
+end
+
+class Person
+  
+  def name=(persons_name)
+    @name = persons_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def job=(jobs_name)
+    @job = jobs_name
+  end
+  
+  def job
+    @job
+  end
+    
+    
+    
+  
+end
